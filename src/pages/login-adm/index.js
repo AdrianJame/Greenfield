df33b62a37a5b282635a36c8d6f74fd5e4906460
pedrolstx <lkz197060@gmail.com>
2023-08-22 17:55:11 -0300

    Terminei a tela login ADM

--- src/pages/login-adm/index.js
+++ src/pages/login-adm/index.js
@@ -9,9 +9,35 @@ export default function LoginADM () {
                 
                 <div className="faixa-1">
                     <p>Admin</p>
-                    <img src="./"></img>
+                    <img src="/assets/images/image 54.svg"></img>
                 </div>
-            
+
+                <div className="faixa-2">
+                    
+                    <div className="linha">
+                        <img src="./assets/images/image 221.png"></img>
+                        <input placeholder="Nome Completo"></input>
+                        <div className="risco"></div>
+                    </div>
+
+                    <div className="linha">
+                        <img src="./assets/images/image 225.png"></img>
+                        <input placeholder="Email"></input> 
+                        <div className="risco"></div>                      
+                    </div>
+
+                    <div className="linha">
+                        <img src="./assets/images/image 221 (1).png"></img>
+                        <input placeholder="Senha"></input>
+                        <div className="risco"></div>                       
+                    </div>
+                    
+                </div>
+
+                <div className="faixa-3">
+                    <button>Entrar</button>
+                </div>
+
             
             </div>
         
