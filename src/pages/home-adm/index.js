3685be78579d5e745b90f5d87ad4dd30acbf74a1
MatthewMagalhaes <magmatheusf@gmail.com>
2023-08-22 18:12:17 -0300

    atualizado

--- src/pages/home-adm/index.js
+++ src/pages/home-adm/index.js
@@ -8,13 +8,13 @@ export default function Homeadm() {
 
         <div className='faixa1'>
             <div className='produtos'>
-                <img src='./assets/img/image 264.svg'></img>
+                <img src='./assets/images/image 264.svg'></img>
                 <h2>PRODUTOS</h2>
                 <p>Cadastre seus produtos e altere-os</p>
             </div>
 
             <div className='analise'>
-                <img src='./assets/img/image 264.svg'></img>
+                <img src='./assets/images/image 264.svg'></img>
                 <h2>ANÁLISE</h2>
                 <p>Confira gráficos com estatísticas sobre suas vendas</p>
             </div>
@@ -24,13 +24,13 @@ export default function Homeadm() {
         <div className='faixa2'>
 
             <div className='status-de-pedidos'>
-                <img src='./assets/img/image 264.svg'></img>
+                <img src='./assets/images/image 264.svg'></img>
                 <h2>STATUS DE PEDIDOS</h2>
                 <p>Confira os pedidos e altere-os</p>
             </div>
 
             <div className='sair'>
-                <img src='./assets/img/image 264.svg'></img>
+                <img src='./assets/im/image 264.svg'></img>
                 <h2>SAIR</h2>
                 <p>Sair ou alterar de conta</p>
             </div>
