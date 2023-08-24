57da59048b73fe7c69322bf41d51ce5a560ec9bf
MatthewMagalhaes <magmatheusf@gmail.com>
2023-08-24 16:44:54 -0300

    ok

--- src/pages/home-adm/index.js
+++ src/pages/home-adm/index.js
@@ -7,37 +7,40 @@ export default function Homeadm() {
 
       <Cabeadm/>
 
-      <div className='conteudo'>
+      <div className='centro'>
 
-        <div className='faixa1'>
-            <div className='produtos'>
-                <img src='./assets/images/image 264.svg'></img>
-                <h2>PRODUTOS</h2>
-                <p>Cadastre seus produtos e altere-os</p>
-            </div>
+        <div className='conteudo'>
 
-            <div className='analise'>
-                <img src='./assets/images/image 264.svg'></img>
-                <h2>ANÁLISE</h2>
-                <p>Confira gráficos com estatísticas sobre suas vendas</p>
-            </div>
+          <div className='faixa1'>
+              <div className='card'>
+                  <img src='./assets/images/image 264.svg'></img>
+                  <h2>PRODUTOS</h2>
+                  <p>Cadastre seus produtos e altere-os</p>
+              </div>
 
-        </div>
+              <div className='card'>
+                  <img src='./assets/images/image 265.svg'></img>
+                  <h2>ANÁLISE</h2>
+                  <p>Confira gráficos com estatísticas sobre suas vendas</p>
+              </div>
 
-        <div className='faixa2'>
+          </div>
 
-            <div className='status-de-pedidos'>
-                <img src='./assets/images/image 264.svg'></img>
-                <h2>STATUS DE PEDIDOS</h2>
-                <p>Confira os pedidos e altere-os</p>
-            </div>
+          <div className='faixa2'>
 
-            <div className='sair'>
-                <img src='./assets/im/image 264.svg'></img>
-                <h2>SAIR</h2>
-                <p>Sair ou alterar de conta</p>
-            </div>
+              <div className='card'>
+                  <img src='./assets/images/image 266.svg'></img>
+                  <h2>STATUS DE PEDIDOS</h2>
+                  <p>Confira os pedidos e altere-os</p>
+              </div>
 
+              <div className='card'>
+                  <img src='./assets/images/image 267.svg'></img>
+                  <h2>SAIR</h2>
+                  <p>Sair ou alterar de conta</p>
+              </div>
+
+          </div>
         </div>
       </div>
     </div>
