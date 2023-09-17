8fa89c4a989d613401a31ce0e123e66864c93806
Matt21x <magmatheusf@gmail.com>
2023-09-17 14:57:59 -0300

    .

--- src/pages/login-adm/index.js
+++ src/pages/login-adm/index.js
@@ -1,6 +1,35 @@
+import { useState } from "react"
+import axios from "axios"
+import { Navigate, useNavigate } from "react-router-dom"
 import "./index.scss"
 
 export default function LoginADM () {
+
+    const[email, setEmail]= useState('');
+    const[senha, setSenha]= useState('');
+    const[erro, setErro]= useState('');
+
+    const Navigate = useNavigate();
+
+    async function entrarClick() {
+        try {
+            const r = await axios.post('',{
+                email: email,
+                senha: senha
+            });
+
+            Navigate('');
+
+            
+        } catch (err) {
+            if(err.response.status === 401){
+                setErro(err.response.data.erro)
+            }
+
+            
+        }
+    }
+
     
     return(
         <div className="principal">
@@ -22,20 +51,20 @@ export default function LoginADM () {
 
                     <div className="linha">
                         <img src="./assets/images/image 225.png"></img>
-                        <input placeholder="Email"></input> 
+                        <input type="text" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}></input> 
                         <div className="risco"></div>                      
                     </div>
 
                     <div className="linha">
                         <img src="./assets/images/image 221 (1).png"></img>
-                        <input placeholder="Senha"></input>
+                        <input type="password" placeholder="Senha" value={senha} onChange={e => setSenha(e.target.value)}></input>
                         <div className="risco"></div>                       
                     </div>
                     
                 </div>
 
                 <div className="faixa-3">
-                    <button>Entrar</button>
+                    <button onClick={entrarClick}>Entrar</button>
                 </div>
 
             
