e59fd77ac2646c8ee20122a00fcadc8c27c9ecee
Matt21x <magmatheusf@gmail.com>
2023-09-17 22:32:48 -0300

    quase

--- src/pages/login-adm/index.js
+++ src/pages/login-adm/index.js
@@ -1,76 +1,77 @@
-import { useState } from "react"
-import axios from "axios"
-import { Navigate, useNavigate } from "react-router-dom"
-import "./index.scss"
+import React, { useState, useRef } from "react";
+import axios from "axios";
+import LoadingBar from "react-top-loading-bar";
+import { useNavigate } from "react-router-dom";
+import "./index.scss";
 
-export default function LoginADM () {
+export default function LoginADM() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [erro, setErro] = useState('');
+  const [carregando, setCarregando] = useState(false);
 
-    const[email, setEmail]= useState('');
-    const[senha, setSenha]= useState('');
-    const[erro, setErro]= useState('');
+  const navigate = useNavigate();
+  const loadingBarRef = useRef(null);
 
-    const Navigate = useNavigate();
+  async function entrar() {
+    setCarregando(true);
+    setErro('');
 
-    async function entrarClick() {
-        try {
-            const r = await axios.post('',{
-                email: email,
-                senha: senha
-            });
+    try {
+      const response = await axios.get('http://localhost:5000/login');
+      const credencial = response.data;
 
-            Navigate('');
+      if (nome === credencial.nome && email === credencial.email && senha === credencial.senha) {
+        // Redirect to the admin home page on successful login
+        navigate('/homeadm');
 
-            
-        } catch (err) {
-            if(err.response.status === 401){
-                setErro(err.response.data.erro)
-            }
-
-            
-        }
+      } else {
+        setErro('⚠ Login ou senha incorretos');
+      }
+    } catch (error) {
+      console.error('⚠ Erro ao verificar as credenciais:', error);
+      setErro('⚠ Erro ao verificar as credenciais. Tente novamente mais tarde.');
+    } finally {
+      setCarregando(false);
     }
+  }
 
-    
-    return(
-        <div className="principal">
-            
-            <div className="card-login">
-                
-                <div className="faixa-1">
-                    <p>Admin</p>
-                    <img src="/assets/images/image 54.svg"></img>
-                </div>
+  return (
+    <div className="principal">
+      <LoadingBar color='#f11946' ref={loadingBarRef} />
 
-                <div className="faixa-2">
-                    
-                    <div className="linha">
-                        <img src="./assets/images/image 221.png"></img>
-                        <input placeholder="Nome Completo"></input>
-                        <div className="risco"></div>
-                    </div>
-
-                    <div className="linha">
-                        <img src="./assets/images/image 225.png"></img>
-                        <input type="text" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}></input> 
-                        <div className="risco"></div>                      
-                    </div>
-
-                    <div className="linha">
-                        <img src="./assets/images/image 221 (1).png"></img>
-                        <input type="password" placeholder="Senha" value={senha} onChange={e => setSenha(e.target.value)}></input>
-                        <div className="risco"></div>                       
-                    </div>
-                    
-                </div>
-
-                <div className="faixa-3">
-                    <button onClick={entrarClick}>Entrar</button>
-                </div>
-
-            
-            </div>
-        
+      <div className="card-login">
+        <div className="faixa-1">
+          <p>Admin</p>
+          <img src="/assets/images/image 54.svg" alt="Admin Logo" />
         </div>
-    )
 
+        <div className="faixa-2">
+          <div className="linha">
+            <img src="./assets/images/image 221.png" alt="Nome Icon" />
+            <input type="text" placeholder="Nome Completo" value={nome} onChange={e => setNome(e.target.value)} />
+            <div className="risco"></div>
+          </div>
+
+          <div className="linha">
+            <img src="./assets/images/image 225.png" alt="Email Icon" />
+            <input type="text" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+            <div className="risco"></div>
+          </div>
+
+          <div className="linha">
+            <img src="./assets/images/image 221 (1).png" alt="Senha Icon" />
+            <input type="password" placeholder="Senha" value={senha} onChange={e => setSenha(e.target.value)} />
+            <div className="risco"></div>
+          </div>
+        </div>
+
+        <div className="faixa-3">
+          <button onClick={entrar} disabled={carregando}>Entrar</button>
+          <p>{erro}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
