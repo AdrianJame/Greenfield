225ee9af43af389db01a4a4f8d871f06d5ed996d
pedrolstx <lkz197060@gmail.com>
2023-10-17 16:57:22 -0300

    pagina restante feita faltando apenas o responsivo

--- src/pages/opções-pagamento/index.js
+++ src/pages/opções-pagamento/index.js
@@ -3,75 +3,87 @@ import Cabecalhocomlogin from '../../components/cabcomlogin'
 import RodapeGreenfield from '../../components/rodape'
 
 export default function Pag () {
+    
     return (
         
         <div className='page-forma-de-pagamento'>
             
             <Cabecalhocomlogin/>
+
+            <h1>Pagamento</h1>
             
-                <h1>Pagamento</h1>
+            <div className='status-pedido'>
 
-                <div className='status-pedido'>
-
-                    <div className='circle-verde'>
-                        <img src='./assets/images/carrinho-status.svg'/>
-                    </div>
+                <div className='circle-verde'>
+                    <img src='./assets/images/carrinho-status.svg'/>
+                </div>
                     <div className='linha-verde'></div>
 
-                    <div className='circle-verde'>
-                        <img src='./assets/images/ping-verde.svg'/>
-                    </div>
+                <div className='circle-verde'>
+                    <img src='./assets/images/ping-verde.svg'/>
+                </div>
                     <div className='linha-verde'></div>
 
-                    <div className='circle-verde'>
-                        <img src='./assets/images/icon-dinheiro-verde.svg'/>
-                    </div>
+                <div className='circle-verde'>
+                    <img src='./assets/images/icon-dinheiro-verde.svg'/>
                 </div>
 
-                <div className='section-pagamento'>
-                
-                    <div className='sub-section-pagamento'>
-
-                        <div className='frete-e-pagamento'>
-                            
-                            <div className='cont-esq'>
-                                
-                                <div className='circle-verde'>
-                                    <img src='./assets/images/ping-verde.svg'/>
-                                </div>
-                                
-                                <div className='desc-frete'>
-                                    <p>Frete:</p>
-                                    <p>Entrega em até</p>
-                                </div>
-
-                            </div>
-
-                            <h1>FORMA DE PAGAMENTO</h1>
-
-                            <div className='cont-dir'>
-                                <a>Editar</a>
-                            </div>
-
-                        </div>
-
-                        <div className='pagamento'>
-                            
-                        </div>
-
-                    </div>
-
-                    <div className='options-pagamento'>
-                        <img src='./assets/images/ping-verde.svg'/>
-                    </div>
-
-                </div>
-
-            <div className='rodape'>
-                <RodapeGreenfield/>
             </div>
-            
-            
+
+            <div className='section-pagamento'>
+
+                <div className='frete-e-pagamento'>
+
+                    <div className='cont-esq'>
+
+                        <div className='circle-verde'>
+                            <img src='./assets/images/ping-verde.svg'/>
+                        </div>
+
+                        <div className='desc-frete'>
+                            <p>Frete:</p>
+                            <p>Entrega em até</p>
+                        </div>
+
+                    </div>
+
+                    <div className='cont-dir'>
+                        <a>Editar</a>
+                    </div>
+
+                </div>
+
+                <h1>ESCOLHA A FORMA DE PAGAMENTO</h1>
+
+                <div className='escolha-pagamento'>
+
+                    <div className='select-pagamento'>
+                        
+                        <div className='conteudo-pagamento-esq'>
+                            <img src='./assets/images/image-cartao-credito.svg'/>
+                            <p>PAGUE COM CARTÃO</p>
+                        </div>
+                        
+                        <div className='conteudo-pagamento-seletor'>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className='conclusão'>
+                
+                <div className='pagamento'>
+                    
+                </div>
+
+            </div>
+
+            <RodapeGreenfield/>
+                
         </div>
     )
 }
