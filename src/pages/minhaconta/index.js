327f2f847ea72212a64c55fb6715d9ec8740cfe0
adrianjame <adrianjamesbadwolf@gmail.com>
2023-11-02 17:15:48 -0300

    so fazer as funções

--- src/pages/minhaconta/index.js
+++ src/pages/minhaconta/index.js
@@ -30,6 +30,30 @@ export default function Minhaconta(){
           });
     }
 
+    function Alterarinfo() {
+        const editar = document.getElementById('fundo-alterarinfo')
+        editar.classList.add('aparecer')
+    
+      }
+    
+      function removerinfo() {
+        const editar = document.getElementById('fundo-alterarinfo')
+        editar.classList.remove('aparecer')
+    
+      }
+
+      function Vercadastro() {
+        const editar = document.getElementById('fundo-vercadastro')
+        editar.classList.add('aparecer')
+    
+      }
+    
+      function remover() {
+        const editar = document.getElementById('fundo-vercadastro')
+        editar.classList.remove('aparecer')
+    
+      }
+
     return(
         <div className='minhaconta'>
             <Cabesemdgd/>
@@ -39,16 +63,16 @@ export default function Minhaconta(){
                         <div className='linha'>
 
                             <div className='agrup'>
-                                <Link to={'/meucadastro'} className='card'>
+                                <a onClick={Vercadastro} to={'/meucadastro'} className='card'>
                                 <img className='imagemcard' src='./assets/images/image 227.svg'/>
-                                </Link>
+                                </a>
                                 <p className='titulo-card'>Meu Cadastro</p>
                             </div>
 
                             <div className='agrup'>
-                                <Link className='card'>
+                                <a onClick={Alterarinfo} className='card'>
                                     <img className='imagemcard' src='./assets/images/image 230 (1).svg' />
-                                </Link>
+                                </a>
                                 <p className='titulo-card'> Alterar Informações</p>
                             </div>
 
@@ -87,6 +111,19 @@ export default function Minhaconta(){
                         </div>
 
                     </div>
+                    
+                    <section className='fundo-alterarinfo' id='fundo-alterarinfo'>
+                        <div className='alterarinfo'>
+                            <button onClick={removerinfo}>fffff</button>
+                        </div>
+                    </section>
+
+                    <section className='fundo-vercadastro' id='fundo-vercadastro'>
+                        <div className='vercadastro'>
+                        <button onClick={remover}>ssss</button>
+                        </div>
+                    </section>
+
             <RodapeGreenfield/>
         </div>
     )
